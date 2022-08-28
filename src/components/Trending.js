8b164328822928f4affd2e71e92d59ff9de06c69
Eden-08 <kusmayantoeden@gmail.com>
2022-08-28 18:40:54 +0700

    Selesai

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { Card, Container, Row, Col, Image } from "react-bootstrap";
-import duneImage from "../assets/images/trending/dune.jpg";
-import everythingImage from "../assets/images/trending/everything.jpg";
-import infiniteImage from "../assets/images/trending/infinite.jpg";
-import lightyearImage from "../assets/images/trending/lightyear.jpg";
-import jokerImage from "../assets/images/trending/joker.jpg";
-import morbiusImage from "../assets/images/trending/morbius.jpg";
+import axios from "axios";
 const Trending = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/discover/movie`, {
+        params: {
+          api_key: process.env.REACT_APP_TMDB_KEY,
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      });
+  }, []);
+
   return (
     <div>
       <Container>
@@ -13,78 +22,22 @@ const Trending = () => {
         <h1 className="text-white">Trending Movie</h1>
         <br />
         <Row>
-          <Col md={4} className="MovieWrapper" id="trending">
-            <Card className="movieImage">
-              <Image src={duneImage} alt="Dune Movie" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">Dune</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content.</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="movieImage">
-              <Image src={everythingImage} alt="Dune Movie" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">Everything Everywhere</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content.</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="movieImage">
-              <Image src={infiniteImage} alt="Dune Movie" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">Invinite</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content.</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="movieImage">
-              <Image src={lightyearImage} alt="Dune Movie" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">LightYear</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content.</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="movieImage">
-              <Image src={jokerImage} alt="Dune Movie" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">Joker</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content.</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
-          <Col md={4} className="MovieWrapper">
-            <Card className="movieImage">
-              <Image src={morbiusImage} alt="Dune Movie" className="images" />
-              <div className="bg-dark">
-                <div className="p-2 m-1 text-white">
-                  <Card.Title className="text-center">Morbius</Card.Title>
-                  <Card.Text className="text-left">This is a wider card with supporting text below as a natural lead-in to additional content.</Card.Text>
-                  <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
-                </div>
-              </div>
-            </Card>
-          </Col>
+          {movies.map((result, index) => {
+            return (
+              <Col md={4} className="MovieWrapper" id="trending" key={index}>
+                <Card className="movieImage">
+                  <Image src={`${process.env.REACT_APP_IMG_URL}/$ {result.poster_path }`} alt="Dune Movie" className="images" />
+                  <div className="bg-dark">
+                    <div className="p-2 m-1 text-white">
+                      <Card.Title className="text-center">{result.title}</Card.Title>
+                      <Card.Text className="text-left">{result.overview}</Card.Text>
+                      <Card.Text className="text-left">{result.release_date}</Card.Text>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </div>
